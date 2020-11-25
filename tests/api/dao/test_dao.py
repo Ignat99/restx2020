c4397ee29647d5a3ffdd24ebe75908853dc53958
Ubuntu <ubuntu@virtualserver01.NetHealth-Lab-LLC.cloud>
2020-11-25 22:21:14 +0000

    pytest DAO 2

--- tests/api/dao/test_dao.py
+++ tests/api/dao/test_dao.py
@@ -6,13 +6,38 @@ from hamcrest import has_entries, has_property, equal_to, \
 all_of, contains_string, assert_that, contains
 from passnfly.api.dao.todo import TodoDAO
 
+
+#def teardown():
+#def setup():
+#    print(DAO.get(1))
+
 def test_create():
     DAO = TodoDAO()
     DAO.create({'name': 'passnfly'})
-    print(DAO.get(1))
-
     a_str = json.dumps(DAO.get(1), sort_keys=True)
     b_str = json.dumps(json.loads("""
 {"name": "passnfly", "id": 1}
 """), sort_keys=True)
-    assert_that(a_str, b_str)
+    assert_that(a_str, equal_to(b_str))
+
+def test_update():
+    DAO = TodoDAO()
+    DAO.create({'name': 'passnfly'})
+    DAO.update(1, {'name': 'passnfly1'})
+    print(DAO.get(1))
+
+    a_str = json.dumps(DAO.get(1), sort_keys=True)
+    b_str = json.dumps(json.loads("""
+{"name": "passnfly1", "id": 1}
+"""), sort_keys=True)
+    assert_that(a_str, equal_to(b_str))
+
+def test_delete():
+    DAO = TodoDAO()
+    DAO.create({'name': 'passnfly'})
+    DAO.delete(1)
+    a_str = json.dumps(DAO.get(1), sort_keys=True)
+    b_str = json.dumps(json.loads("""
+{"name": "passnfly", "id": 1}
+"""), sort_keys=True)
+    assert_that(a_str, equal_to(b_str))
