88433c26d9c58d8059d6f7ebb09930f7dc09c7f0
Ubuntu <ubuntu@virtualserver01.NetHealth-Lab-LLC.cloud>
2020-11-25 01:16:14 +0000

    Resources pylint check

--- passnfly/api/resources/todo.py
+++ passnfly/api/resources/todo.py
@@ -1,16 +1,10 @@
 #!/usr/bin/env python3
-
+"""REST API for keep/shared data"""
 from __future__ import print_function
-import sys
-import json
-from flask import Flask
-#from flask_restx import Api, Resource, fields
-from flask_restx import Resource, marshal, Namespace
-#from flask_sqlalchemy import SQLAlchemy
-#from sqlalchemy import func
+from flask_restx import Resource, Namespace
 from passnfly.api.dao.todo import TodoDAO
 from passnfly.api.aiokafka.todo import post_data2
-from passnfly.api.models.todo import *
+from passnfly.api.models.todo import api, ns, todo, add_models_to_namespace
 
 
 todo_ns = Namespace('Todo', description='Operations related to todo')
@@ -26,6 +20,7 @@ class TodoList(Resource):
     def get(self):
         '''List all tasks'''
         topic=post_data2('list_todos')
+        print("We use topic: " + topic)
         return DAO.todos
 
     @ns.doc('create_todo')
@@ -43,19 +38,19 @@ class Todo(Resource):
     '''Show a single todo item and lets you delete them'''
     @ns.doc('get_todo')
     @ns.marshal_with(todo)
-    def get(self, id):
+    def get(self, id1):
         '''Fetch a given resource'''
-        return DAO.get(id)
+        return DAO.get(id1)
 
     @ns.doc('delete_todo')
     @ns.response(204, 'Todo deleted')
-    def delete(self, id):
+    def delete(self, id1):
         '''Delete a task given its identifier'''
-        DAO.delete(id)
+        DAO.delete(id1)
         return '', 204
 
     @ns.expect(todo)
     @ns.marshal_with(todo)
-    def put(self, id):
+    def put(self, id1):
         '''Update a task given its identifier'''
-        return DAO.update(id, api.payload)
+        return DAO.update(id1, api.payload)
