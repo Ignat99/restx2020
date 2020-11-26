1b06a4005f7430a04482039c4f9a8e1adadb443d
Ubuntu <ubuntu@virtualserver01.NetHealth-Lab-LLC.cloud>
2020-11-26 01:03:32 +0000

    kafka sensor

--- tests/api/aiokafka/test_cons.py
+++ tests/api/aiokafka/test_cons.py
@@ -1,15 +1,18 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 from confluent_kafka import Consumer, KafkaError
 
-c = Consumer({'bootstrap.servers': '192.168.99.100:9092', 'group.id': 'mygroup', 'default.topic.config': {'auto.offset.reset': 'smallest'}})
-c.subscribe(['8e45c76c6d04666f047a204cb9566c4b'])
+c = Consumer({'bootstrap.servers': 'localhost:9092', 'group.id': 'mygroup', 'default.topic.config': {'auto.offset.reset': 'smallest'}})
+c.subscribe(['test'])
 
-running = True
-while running:
+#running = True
+counter = 2
+#while running:
+while counter:
+    counter = counter - 1
     msg = c.poll()
     if not msg.error():
         print('Received message: %s' % msg.value().decode('utf-8'))
     elif msg.error().code() != KafkaError._PARTITION_EOF:
         print(msg.error())
-        running = False
+#        running = False
         c.close()
