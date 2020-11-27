0eb341ebb3426d143c56017be66607097456779d
Ubuntu <ubuntu@virtualserver01.NetHealth-Lab-LLC.cloud>
2020-11-27 01:18:34 +0000

    pytest db sqlalchemy extension 2

--- passnfly/db/sqlalchemy_extension.py
+++ passnfly/db/sqlalchemy_extension.py
@@ -5,10 +5,15 @@ from sqlalchemy import MetaData
 db = SQLAlchemy(
     metadata=MetaData(
         naming_convention={
+            # Именование первичных ключей
             "pk": "pk_%(table_name)s",
+            # Именование внешних ключей
             "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+            # Именование индексов
             "ix": "ix_%(table_name)s_%(column_0_name)s",
+            # Именование уникальных индексов
             "uq": "uq_%(table_name)s_%(column_0_name)s",
+            # Именование CHECK-constraint-ов
             "ck": "ck_%(table_name)s_%(constraint_name)s",
         }
     )
