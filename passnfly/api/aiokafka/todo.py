17d814b9ed716debdfbfe997638a0e58cd708ac3
ignat99 <ignat99@gmail.com>
2021-01-13 21:39:28 +0000

    transactional.id

--- passnfly/api/aiokafka/todo.py
+++ passnfly/api/aiokafka/todo.py
@@ -7,7 +7,7 @@ from threading import Thread, current_thread
 import hashlib
 import time
 #from kafka import KafkaProducer, KafkaConsumer
-from confluent_kafka import Producer, Consumer
+from confluent_kafka import Producer, Consumer, KafkaError, TopicPartition
 from confluent_kafka.admin import AdminClient, NewTopic
 
 KAFKA_SERVER = "localhost:9092"
@@ -33,25 +33,18 @@ class TodoThread(Thread):
 
         self.topic=topic
         self.keywords=keywords
-        self.producer=Producer({'bootstrap.servers': KAFKA_SERVER,
-            'acks': 'all',
-            'retries': 5
-        })
+        self.transactional_id='todo.py'
 
-#        self.producer= KafkaProducer(bootstrap_servers=KAFKA_SERVER,
-#            acks='all',
-#            retries=5,
-#            )
-
-#            enable_idempotence=True,
-#            max_in_flight_requests_per_connection=1
-#            transactional_id='my-transactional-id'
         try:
             self.consumer = Consumer({'bootstrap.servers': '0.0.0.0:9092',
                 'auto.offset.reset': 'earliest',
                 'group.id': 'ping-pong',
-                'client.id': 'client-1'
+                'client.id': 'client-1',
+                'enable.auto.commit': False,
+                'enable.partition.eof': True
             })
+
+
 
 #            self.consumer = KafkaConsumer(bootstrap_servers=['0.0.0.0:9092'],
 #                auto_offset_reset='latest',
@@ -71,8 +64,43 @@ class TodoThread(Thread):
 
         try:
             self.consumer.subscribe(['df911f0151f9ef021d410b4be5060972'])
+            # Prior to KIP-447 being supported each input partition requires
+            # its own transactional producer, so in this example we use
+            # assign() to a single partition rather than subscribe().
+            # A more complex alternative is to dynamically create a producer per
+            # partition in subscribe's rebalance callback.
+#            self.consumer.assign([TopicPartition('df911f0151f9ef021d410b4be5060972', self.input_partition)])
         except:
             print("Error: For Kafka need partition test (Need run ./tweeterapi.py)")
+
+
+
+
+        self.producer=Producer({'bootstrap.servers': KAFKA_SERVER,
+            'acks': 'all',
+            'retries': 5,
+            'transactional.id': self.transactional_id
+        })
+
+#        self.producer= KafkaProducer(bootstrap_servers=KAFKA_SERVER,
+#            acks='all',
+#            retries=5,
+#            )
+
+#            enable_idempotence=True,
+#            max_in_flight_requests_per_connection=1
+
+        # Initialize producer transaction.
+#        self.producer.init_transactions()
+        # Start producer transaction.
+#        self.producer.begin_transaction()
+
+        self.eof = {}
+        self.msg_cnt = 0
+        self.input_partition = 0
+
+
+
 
     def next_tuple(self):
         """Take next json message object"""
@@ -120,11 +148,11 @@ class TodoThread(Thread):
         if err is not None:
             print('Message delivery failed: {}'.format(err))
         else:
-            print('Message delivered to {} [{}]'.format(msg.topic(), msg.partition()))
+            print('Message delivered to {} [{}]'.format(msg.topic(), str(msg.partition()), err))
 
 
     def ping_send(self,topic, hashtags,max_items=0):
-        item = { 'transaction-id': 'TRANSACTION-ID',
+        item = { 'transaction-id': self.transactional_id,
             'payload': {
                 'message': 'ping',
                 'force_error': 'false'
@@ -140,7 +168,7 @@ class TodoThread(Thread):
         print("async ping data transfer started...")
 
     def pong_send(self,topic, hashtags,max_items=0):
-        item = { 'transaction-id': 'TRANSACTION-ID',
+        item = { 'transaction-id': self.transactional_id,
             'payload': {
                 'message': 'pong',
                 'force_error': 'false'
@@ -157,26 +185,63 @@ class TodoThread(Thread):
 
     def body(self):
         """Wakeup and make something every REPEAT_DELAY_SEC"""
+        print("=== Starting Consume-Transform-Process loop ===")
         while True:
-            print("Thread: Todo request " + self.name)
+            print("Thread: Todo request {} -  {} ".format(self.name, str(self.msg_cnt)))
+            # serve delivery reports from previous produce()s
+            self.producer.poll(0)
+                    #self.next_tuple()
+#                    message=next(self.consumer)
+
+            # read message from input_topic
+            message = self.consumer.poll(timeout=1.0)
+            if message is None:
+                continue
+
+            msg_topic, msg_partition = message.topic(), message.partition()
+            if message.error():
+                if message.error().code() == KafkaError._PARTITION_EOF:
+                    self.eof[(msg_topic, msg_partition)] = True
+                    print("=== Reached the end of {} [{}] at {}====".format(
+                        msg_topic, msg_partition, message.offset()))
+
+                    if len(eof) == len(self.consumer.assignment()):
+                        print("=== Reached end of input ===")
+                        break
+                continue
+            # clear EOF if a new message has been received
+            self.eof.pop((msg_topic, msg_partition), None)
+            self.msg_cnt += 1
+
+            # process message
             try:
                 if (self.name == 'df911f0151f9ef021d410b4be5060972'):
                     self.ping_send(self.topic, self.keywords, POSTS_LIMIT_FOR_HASHTAG)
 
                 else:
-                    #self.next_tuple()
-#                    message=next(self.consumer)
-                    message = self.consumer.poll(1.0)
-                    if message is None:
-                        continue
-                    if message.error():
-                        print("Consumer error: {}".format(message.error()))
-                        continue
-
                     ping_dict = json.loads(message.value().decode('utf-8'))
                     print('Ping : ' , ping_dict['payload']['force_error'])
+
+                    # produce transformed message to output topic TODO: dead_letter topic
                     if ping_dict['payload']['force_error'] == 'false':
                         self.pong_send(self.topic, self.keywords, POSTS_LIMIT_FOR_HASHTAG)
+
+                    if self.msg_cnt % 100 == 0:
+                        print("=== Committing transaction with {} messages at input offset {} ===".format(
+                            self.msg_cnt, message.offset()))
+                        # Send the consumer's position to transaction to commit
+                        # them along with the transaction, committing both
+                        # input and outputs in the same transaction is what provides EOS.
+#                        self.producer.send_offsets_to_transaction(
+#                            self.consumer.position(self.consumer.assignment()),
+#                            self.consumer_group_metadata())
+
+                        # Commit the transaction
+#                        self.producer.commit_transaction()
+
+                        # Begin new transaction
+#                        self.producer.begin_transaction()
+                        self.msg_cnt = 0
 
             except Exception as err1:
                 if FORCE_RAISE:
@@ -188,6 +253,17 @@ class TodoThread(Thread):
 
             time.sleep(REPEAT_DELAY_SEC)
 
+
+        print("=== Committing final transaction with {} messages ===".format(self.msg_cnt))
+        # commit processed message offsets to the transaction
+#        self.producer.send_offsets_to_transaction(
+#            self.consumer.position(self.consumer.assignment()),
+#            self.consumer_group_metadata())
+
+        # commit transaction
+#        self.producer.commit_transaction()
+
+        self.consumer.close()
 
 def topic_from_keywords(keywords):
     """Make a hash from set of keywords and data and lang etc"""
